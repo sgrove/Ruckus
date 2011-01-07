285b7a3fc4cbeb6a7cd60ca6c8abb54dec665b41
Kev Zettler <kevzettler@gmail.com>
2011-01-07 11:28:34 -0800

    added some conditinal layouts to devise for install/admin stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :setpages
+  layout :layout_by_resource
   
   def setpages
     @pages = Page.all
+  end
+  
+  def layout_by_resource
+    if devise_controller?
+      "admin"
+    end
   end
 end
